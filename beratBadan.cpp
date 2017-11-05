059026585d4646c755da2847b1b123d932106eba
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2017-11-05 15:37:11 -0800

    menginput tinggi dan berat badan

--- beratBadan.cpp
+++ beratBadan.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main (){
+	float tinggi, berat, hasil;
+	
+	cout << "Masukkan Tinggi Badan (cm) : ";
+	cin >> tinggi;
+	cout << "Masukkan Berat Badan (kg) : ";
+	cin >> berat;
+	cout << "Berat Badan anda adalah : " << endl;
+	
 	
 }
