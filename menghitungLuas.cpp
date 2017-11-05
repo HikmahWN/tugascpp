f125a2d01c4606bfb3c045dd9c6d70022ea76ef5
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2017-11-05 15:51:25 -0800

    menghitung persegi panjang

--- menghitungLuas.cpp
+++ menghitungLuas.cpp
@@ -6,9 +6,19 @@ int main() {
 	int input;
 	
 	cout << "Pilihan Hitungan\n";
-	cout << "   1. P";
-	cout << "   2. S";
+	cout << "   1. P\n";
+	cout << "   2. S\n";
 	cin >> input;
 	
-	
+	switch (input)
+	{
+		case 1 : 
+			cout << "Masukkan Panjang : ";
+			cin >> panjang;
+			cout << "Masukkan Lebar : ";
+			cin >> lebar;
+			hasil = panjang * lebar;
+			cout << "Luas Persegi Panjang adalah : " << hasil << endl;
+			break;
+	}
 }
