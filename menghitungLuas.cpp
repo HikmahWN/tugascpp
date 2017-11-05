3236e34d9c2ea417a36528338b216ce68f000edc
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2017-11-05 15:46:29 -0800

    menentukan pilihan untuk hitungan

--- menghitungLuas.cpp
+++ menghitungLuas.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+	float alas, tinggi, hasil, panjang, lebar;
+	int input;
+	
+	cout << "Pilihan Hitungan\n";
+	cout << "   1. P";
+	cout << "   2. S";
+	cin >> input;
+	
 	
 }
